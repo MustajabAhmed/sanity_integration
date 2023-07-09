592f9915ce118542de9096d015d3cadea857b6f1
MustajabAhmed <mustajabahmed02@gmail.com>
2023-07-09 15:26:01 +0500

    Sanity Integration done for products

---
+++ sanity/product.ts
@@ -0,0 +1,22 @@
+export const product = {
+    name: 'product',
+    type: 'document',
+    title: 'Product',
+    fields: [
+        {
+            name: 'title',
+            title: 'Product Title',
+            type: 'string'
+        },
+        {
+            name: 'description',
+            title: 'Product Description',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Product Image',
+            type: 'image'
+        },
+    ]
+}
